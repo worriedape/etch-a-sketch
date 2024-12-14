93ab430dd4b593018a70e87626e78b5f85b7e960
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-14 07:23:03 -0600

    Create container and its child appended to the HTML body

---
+++ script.js
@@ -0,0 +1,18 @@
+// generate default grid square density (16x16)
+// grid shoud be composed of square divs, preferably without borders
+// only the container of these divs has a border
+// use Flexbox to achieve this
+const body = document.querySelector("body");
+
+const container = document.createElement("div");
+body.appendChild(container);
+
+const pixel = document.createElement("div");
+container.appendChild(pixel);
+
+
+// modify grid square density via prompt or a slide bar to a maximum of 100x100
+
+// squares should change its color fill upon mouse hover and keep that fill after the pointer is away
+// this color fill should change upon repeating mouse hovers
+// eg. let's say they get 10% darker every pass (check CSS opacity property)
