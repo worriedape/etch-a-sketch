02fa10247bc32f9e393be0fed5a6a3c0c1c032ba
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-16 11:48:34 -0600

    add eventlistener mouseenter to change the color of each pixel as mouse hovers over it

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 // use Flexbox to achieve this
 const body = document.querySelector("body");
 
+//const density = document.getElementById("density");
+//let currentVal = density.value;
+
 const container = document.createElement("div");
 container.classList.add("container");
 body.appendChild(container);
@@ -11,19 +14,27 @@ body.appendChild(container);
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment
 const fragment = document.createDocumentFragment();
 
+// get the math right. What's causing the pixels not fitting
+// precisely within the flex container without hard coding its values?
+// check percentages
+
+/*let percentage = (fill - 100) / (100) * 100;*/
+
 let i = 1;
 let fill = 64;
-let x =  fill * fill;
+let x = fill * fill;
 // create pixel x times for row (X)
 for (; i <= x; i++) {
   //container.append(pixel);
   const pixel = document.createElement("div");
   pixel.classList.add("pixel");
+  pixel.addEventListener("mouseenter", () => {
+    pixel.style.backgroundColor = "black";
+  });
   fragment.appendChild(pixel);
 }
 
 container.appendChild(fragment);
-
 
 // create pixel x times for column (Y)
 
